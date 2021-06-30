ccdef6f6866ce55553641cc008154df233d2ddb1
juangpc <juangpc@gmail.com>
2021-06-30 17:44:29 -0400

    add main user interface script

---
+++ fieldline_api/mne_fieldline.py
@@ -0,0 +1,23 @@
+import mne_fieldline_lib as lib
+
+       
+if __name__ == "__main__":
+    opm = FieldlineDevide()
+    
+    continue_loop = True
+    
+    while(continue_loop):
+        print("Commands:")
+        print("\tStart Measurment - start")
+        print("\tStop Measurement - stop")
+        print("\tDisconnect and exit - exit")
+        command = input("Select command: ")
+        if command == "start":
+            print("Starting measurement...")
+            opm.start_measurement()
+        elif command == "stop":
+            print("Stopping measurement...")
+            opm.stop_measurement()
+        elif command == "exit":
+            print("Exiting program.")
+            continue_loop = False
