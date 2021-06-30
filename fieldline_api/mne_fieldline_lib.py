e4f07b251ffa325787b229f702659a9bcec88fb9
juangpc <juangpc@gmail.com>
2021-06-30 17:45:04 -0400

    add fieldline backend api

---
+++ fieldline_api/mne_fieldline_lib.py
@@ -0,0 +1,34 @@
+import queue
+import time
+import threading
+import numpy
+
+import mne_fieldline_config as config
+import FieldTrip
+
+
+class FieldlineDevice:
+    def __init__(self):
+        self.connect()
+        self.init_sensors()
+
+    def __del__(self):
+        self.stop_measurement()
+        self.disconnect()
+
+    def connect(self):
+        lib.init_fieldline_connection()
+        lib.init_fieldtrip_connection()
+        lib.init_sensors()
+
+    def start_measurement(self):
+        if lib.are_sensors_ready():
+            lib.init_acquisition()
+        else:
+        print("Sensors are not initialized")
+        
+    def stop_measurement(self):
+        lib.stop_measurement()
+
+    def disconnect(self):
+        lib.stop_service()
