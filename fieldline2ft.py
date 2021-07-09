90cd04bbae39ad79dbb5193f56b2b03290ddab77
juangpc <juangpc@gmail.com>
2021-07-09 14:43:33 -0400

    update gitignore

--- fieldline2ft.py
+++ fieldline2ft.py
@@ -3,3 +3,8 @@ import lib.application
 config = 'fieldline2ft.ini'
 app = lib.application.App(config_file = config)
 app.start()
+
+def do_thing_2():
+    pass
+
+app.gui_model.callback_list[2] = do_thing_2
