7bd64047ec2c4f083fbfee018ce2c72f4ade410b
juangpc <juangpc@gmail.com>
2021-07-10 02:42:23 -0400

    add thread safet to application

--- lib/application.py
+++ lib/application.py
@@ -1,12 +1,12 @@
 import time
-
-
+import threading
 import lib.gui
 
 class App:
     def __init__(self):
-        self.exit_application = False
         self.time_step = 0.01
+        self.__exit_app = False
+        self.__exit_app_lock = threading.Lock()
         self.__init_gui()
 
     def __del__(self):
@@ -17,10 +17,22 @@ class App:
 
     def exit(self):
         self.gui.exit_loop()
-        self.exit_application = True
+        self.__exit_application(True)
+
+    def __exit_application(self, *argv):
+        if len(argv) == 0:
+            self.__exit_app_lock.acquire()
+            out_state = self.__exit_app
+            self.__exit_app_lock.release()
+        if len(argv) == 1 and type(argv[0]) is bool:
+            self.__exit_app_lock.acquire()
+            self.__exit_app = argv[0]
+            self.__exit_app_lock.release()
+            out_state = argv[0]
+        return out_state
 
     def __loop(self):
-        while not self.exit_application:
+        while not self.__exit_application():
             self.gui.update(self.gui_model)
             time.sleep(self.time_step)
 
