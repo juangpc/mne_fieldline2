a7fbb6e2b2b580ac37932cd31bb47af899181936
juangpc <juangpc@gmail.com>
2021-07-10 02:02:12 -0400

    comment out menus for now

--- fieldline2ft.py
+++ fieldline2ft.py
@@ -9,19 +9,19 @@ app = lib.application.App()
 #     mne.disconnect
 #     app.exit
 
-# menu_item_connect = lib.gui.MenuItem('Connect to Fieldline', mne.connect)
-# menu_item_disconnect = lib.gui.MenuItem('Disconnect from Fieldline', mne.disconnect)
+# menu_item_connect = lib.gui.Menu_Item('Connect to Fieldline', mne.connect)
+# menu_item_disconnect = lib.gui.Menu_Item('Disconnect from Fieldline', mne.disconnect)
 
-# menu_item_start = lib.gui.MenuItem('Start to Fieldline', mne.start_measurement)
-# menu_item_stop = lib.gui.MenuItem('Connect to Fieldline', mne.stop_measurement)
+# menu_item_start = lib.gui.Menu_Item('Start to Fieldline', mne.start_measurement)
+# menu_item_stop = lib.gui.Menu_Item('Connect to Fieldline', mne.stop_measurement)
 
-# menu_item_tune_sensors = lib.gui.MenuItem('Tune Fieldline sensors', mne.tune_sensors)
-# menu_item_stop_tuning_sensors = lib.gui.MenuItem('Stop tuning sensors', mne.stop_tune_sensors)
+# menu_item_tune_sensors = lib.gui.Menu_Item('Tune Fieldline sensors', mne.tune_sensors)
+# menu_item_stop_tuning_sensors = lib.gui.Menu_Item('Stop tuning sensors', mne.stop_tune_sensors)
 
-# menu_item_use_phantom = lib.gui.MenuItem('Use Phantom', mne.phantom_on)
-# menu_item_dont_use_phantom = lib.gui.MenuItem('Use Real Device', mne.phantom_off)
+# menu_item_use_phantom = lib.gui.Menu_Item('Use Phantom', mne.phantom_on)
+# menu_item_dont_use_phantom = lib.gui.Menu_Item('Use Real Device', mne.phantom_off)
 
-# menu_item_exit = lib.gui.MenuItem('Exit', exit)
+# menu_item_exit = lib.gui.Menu_Item('Exit', exit)
 
 # menu = [menu_item_connect]
 
