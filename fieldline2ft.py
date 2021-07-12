1e6fcd935db9e6a6e534d64c3794160642688025
juangpc <juangpc@gmail.com>
2021-07-12 16:21:26 -0400

    clean unecessary dependencies

--- fieldline2ft.py
+++ fieldline2ft.py
@@ -1,8 +1,41 @@
 import lib.application
+import mne_config_file_parser
 
-config = 'fieldline2ft.ini'
+config_file = 'fieldline2ft.ini'
+config = mne_config_file_parser.parse_config_file(config_file)
 
 app = lib.application.App()
+
+
+
+
+ft_client = FieldTrip.Client()
+ft_IP = config.ft_IP
+ft_port = config.ft_port
+ft_data_type = FieldTrip.DATATYPE_FLOAT32
+
+data_stream_multiplier = 1
+
+def connect_to_fieldtrip_buffer():
+    ft_client.connect(ft_IP, ft_port)
+    if ft_client.isConnected:
+        print("Fieldtrip Client connected")
+
+def init_ft_header():
+    if ft_client.isConnected:
+        ft_client.putHeader(num_working_sensors(), default_sample_freq, ft_data_type)
+        header = ft_client.getHeader()
+        if header.nChannels == num_working_sensors():
+            print("Fieldtrip header initialized")
+
+def test_data_to_ft():
+    arr_data = nunmpy.zeros((200,num_working_sensors()), dtype=numpy.single)
+    ft_client.putData(arr_data)
+    
+def init_fieldtrip_connection():
+    connect_to_fieldtrip_buffer()
+    init_ft_header()                
+
 
 # def exit():
 #     mne.disconnect
